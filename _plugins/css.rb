fead64da64b3218b985e1d0e627900022ed186ee
Brian van Burken <brianvanburken@gmail.com>
2020-09-01 22:14:36 +0200

    Add dynamic destination directory for CSS plugin

--- _plugins/css.rb
+++ _plugins/css.rb
@@ -1,6 +1,11 @@
-Jekyll::Hooks.register(:site, :post_write) do |_site|
+Jekyll::Hooks.register(:site, :post_write) do |site|
   next if ENV['JEKYLL_ENV'] != 'production'
 
+  dest = site.config['destination']
+  stylesheet = Dir.glob("#{dest}/assets/*.css").first
+  puts("Plugin CSS: Processing #{stylesheet}")
+
+  puts("Plugin CSS: Running PurgeCSS")
   # Temp file to store options. Command line would not accept a series of
   # whitelist classes, and there are a few classes purgecss is missing.
   config_file = 'tmp/purgecss.js'
@@ -9,7 +14,6 @@ Jekyll::Hooks.register(:site, :post_write) do |_site|
   # Delete existing config file, if it exists.
   File.delete(config_file) if File.exist?(config_file)
   # Main css stylesheet
-  stylesheet = Dir.glob('build/assets/*.css').first
   # Configuration JS to write to the file. (Docs: https://www.purgecss.com/configuration)
   config_text = """module.exports = #{{
     # Wildcard glob of the site's HTML files.
@@ -26,6 +30,7 @@ Jekyll::Hooks.register(:site, :post_write) do |_site|
   # Clean up
   FileUtils.remove_dir('tmp')
 
+  puts("Plugin CSS: Running CSSO")
   # Run CSSO command to clean up CSS further
   system("csso #{stylesheet} -o #{stylesheet}")
 end
