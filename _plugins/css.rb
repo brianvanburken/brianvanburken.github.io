bd20f5b9b2f78c11f319626d6eec3bdb62902ffa
Brian van Burken <brianvanburken@gmail.com>
2020-09-02 09:29:21 +0200

    Whitelist Prism styling for PurgeCSS

--- _plugins/css.rb
+++ _plugins/css.rb
@@ -4,14 +4,31 @@ Jekyll::Hooks.register(:site, :post_write) do |site|
 
   dest = site.config['destination']
   stylesheet = Dir.glob("#{dest}/assets/*.css").first
-  puts "#{title.rjust(18)}: Processing #{stylesheet}"
 
   puts "#{title.rjust(18)}: Running PurgeCSS"
+  # Temp file to store options. Command line would not accept a series of
+  # whitelist classes, and there are a few classes purgecss is missing.
+  config_file = 'purgecss.config.js'
+  # Make sure the tmp directory exists.
+  # Delete existing config file, if it exists.
+  File.delete(config_file) if File.exist?(config_file)
+  # Configuration JS to write to the file. (Docs: https://www.purgecss.com/configuration)
+  config_text = """module.exports = {
+    content: ['#{dest}/**/*.html'],
+    css: ['#{stylesheet}'],
+    output: '#{stylesheet}',
+    whitelistPatternsChildren: [/^token/, /^pre/, /^code/]
+  }"""
+  # Write configuration file.
+  File.open(config_file, 'w+') { |f| f.write(config_text) }
   # Run purgecss command.
-  system("purgecss --css #{stylesheet} --content #{dest}/**/*.html --output #{stylesheet}")
+  system("purgecss -c #{config_file}")
+  # Delete generated config file
+  File.delete(config_file) if File.exist?(config_file)
 
   puts "#{title.rjust(18)}: Running CSSO"
   # Run CSSO command to clean up CSS further
   system("csso #{stylesheet} -o #{stylesheet}")
+
   puts "#{title.rjust(18)}: Done"
 end
